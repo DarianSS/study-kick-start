c4bf09ceb1e37974a92bb4b78c892b975bb732a1
Darian <darian@Darians-MacBook.local>
2015-04-28 01:26:18 +0100

    Backup

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -7,8 +7,6 @@ $(document).ready(function() {
 
     $('span').html(localStorage.name);
 
-    $(".selection").hide();
-
     ////////////////////////////   Locks & Links   //////////////////////////// 
     for (i=2; i<=localStorage.progress; i++) {
         if (i < 7) {
@@ -35,21 +33,22 @@ $(document).ready(function() {
     }
     
     ////////////////////////////   Mascots Replacement   //////////////////////////// 
+    $(".selection").hide();
+
     $(".table_cell").click(function() {
         $(".selection").hide();
         $(this).each(function() {
-        $(this).children(".selection").show();
+            $(this).children(".selection").show();
             localStorage.mascot = $(this).children().first().attr("src");
             $('.didyouknow').html("<img src='" + localStorage.mascot + "' alt='mascot'>");
         });
     });
 
     ////////////////////////////   Blur   //////////////////////////// 
-    $(".character *").bind("load", function() {
-        $(".characterIntro").hide();
-        //$("#blurred").hide();
+    $(".characterIntro").hide();
+    $(".character *").appear(function() {
         $(function() {
-            $(".character").delay(5500)
+            $(".character").delay(2000)
                             .queue(function() {
                                 $(this).foggy({
                                     blurRadius: 8,  
@@ -64,18 +63,15 @@ $(document).ready(function() {
     });
 
     ////////////////////////////   Quizes   //////////////////////////// 
-    $(".quiz *").click(function() {
-        $(".quiz *").css('background-color', 'transparent');
-        
-        $(this).each(function() {
-            $(this).css('background-color', 'rgba(104, 179, 193, 1)'); 
-            quizAnswer = $(this).attr("id");
-            parent = $(this).parents("[data-role='page']");
-            address = "#" + parent.attr("id") + quizAnswer; 
-            child = $(parent).children("a"); 
-            $(child).attr("href", address);
-        });
-    });
+    $(".quiz #wrong, #right").click(function() {
+        $(".quiz *").css('background-color', 'transparent');        
+        $(this).css('background-color', 'rgba(104, 179, 193, 1)'); 
+        quizAnswer = $(this).attr("id");
+        parent = $(this).parents("[data-role='page']");
+        address = "#" + parent.attr("id") + quizAnswer; 
+        child = $(parent).children("a"); 
+        $(child).attr("href", address);
+    }); 
 
     ////////////////////////////   PHQ   //////////////////////////// 
         localStorage.phq = 0;
@@ -93,22 +89,10 @@ $(document).ready(function() {
                 }
                 previousId = $(this).parents("[data-role='page']").attr("id");
                 previousNumber = +($(this).attr("id"));
+                
                 localStorage.phq = +localStorage.phq + +($(this).attr("id"));
             });
         });
-
-        if (localStorage.phq < 6) {
-            $("#page21 a").attr("href", "#page22A");
-        }
-        else if (localStorage.phq < 11) {
-            $("#page21 a").attr("href", "#page22B");
-        }
-        else if (localStorage.phq < 16) {
-            $("#page21 a").attr("href", "#page22C");
-        }
-        else if (localStorage.phq < 21) {
-            $("#page21 a").attr("href", "#page22D");
-        }
 
     ////////////////////////////   Bad Thoughts Box   ////////////////////////////
     $(".badThoughtsBox *").click(function() { 
@@ -129,9 +113,35 @@ $(document).ready(function() {
         $(this).next().fadeIn("slow");
     });   
 
-    /*$(function() {
-        $(document.body).on("appear", ".character", alert("Appeared!"));
-    }); */
+    ////////////////////////////   Timer   //////////////////////////// 
+    /* $(".timer").TimeCircles({
+        "bg_width": 0.2,
+        "fg_width": 0.03,
+        "count_past_zero": false,
+        "circle_bg_color": "#091a37",
+        "time": {
+            "Days": {
+                "text": "Days",
+                "color": "#FFCC66",
+                "show": false
+            },
+            "Hours": {
+                "text": "Hours",
+                "color": "#99CCFF",
+                "show": false
+            },
+            "Minutes": {
+                "text": "Minutes",
+                "color": "#BBFFBB",
+                "show": false
+            },
+            "Seconds": {
+                "text": "Seconds",
+                "color": "#68b3c1",
+                "show": true
+            }
+        }    
+    }); */     //call me maybe when you want to implement this in the html
 }); 
 
 
